d2ce3cdf96e2f5d4437aa9425ac1bd7e181257dd
Alucherdi <hjgo127942@gmail.com>
2017-08-18 13:21:49 -0500

    Adding route, html, css and js to home page

---
+++ public/js/navbar.js
@@ -0,0 +1,10 @@
+
+var openSidenav = function (e) {
+	var sidenav = document.getElementsByClassName("sidenav")[0]
+	sidenav.style.display = "inline";
+}
+
+var closeSidenav = function (e) {
+	var sidenav = document.getElementsByClassName("sidenav")[0]
+	sidenav.style.display = "none";
+}
--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,15 @@
 <?php
 // Routes
 
-$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+// $app->get('/[{name}]', function ($request, $response, $args) {
+//     // Sample log message
+//     $this->logger->info("Slim-Skeleton '/' route");
 
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+//     // Render index view
+//     return $this->renderer->render($response, 'index.phtml', $args);
+// });
+
+$app->get("/", function($req, $res) {
+	
+	return $this->renderer->render($res, "home.phtml");
 });
