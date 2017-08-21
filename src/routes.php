f622df365ee5e9dabcedf043b11e13bd13341b16
Alucherdi <hjgo127942@gmail.com>
2017-08-21 17:32:44 -0500

    Adding phtml of navbar links

--- src/routes.php
+++ src/routes.php
@@ -9,7 +9,26 @@
 //     return $this->renderer->render($response, 'index.phtml', $args);
 // });
 
-$app->get("/", function($req, $res) {
-	
+$app->get("/", function ($req, $res) {
 	return $this->renderer->render($res, "home.phtml");
+});
+
+$app->get("/about", function ($req, $res) {
+	return $this->renderer->render($res, "about.phtml");
+});
+
+$app->get("/services", function ($req, $res) {
+	return $this->renderer->render($res, "services.phtml");
+});
+
+$app->get("/products", function ($req, $res) {
+	return $this->renderer->render($res, "products.phtml");
+});
+
+$app->get("/projects", function ($req, $res) {
+	return $this->renderer->render($res, "projects.phtml");
+});
+
+$app->get("/board", function ($req, $res) {
+	return $this->renderer->render($res, "board.phtml");
 });
