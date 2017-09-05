ce3cd5fa28353a77c49231ea095a7a20d0a2bde6
Alucherdi <hjgo127942@gmail.com>
2017-09-04 22:44:41 -0500

    Last summary

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
 echo "moving to teambits.mx"
-sshpass -p 'TeamBITS@2016' scp -r ./public ./src ./templates adminteambits@teambits.mx:public_html
+sshpass -p 'TeamBITS@2016' scp -r ./templates adminteambits@teambits.mx:public_html
+sshpass -p 'TeamBITS@2016' scp -r ./public/css adminteambits@teambits.mx:public_html/public
+
+
+#sshpass -p 'TeamBITS@2016' scp -r ./public adminteambits@teambits.mx:public_html
 echo "complete"
--- src/routes.php
+++ src/routes.php
@@ -21,8 +21,32 @@ $app->get("/services", function ($req, $res) {
 	return $this->renderer->render($res, "services.phtml");
 });
 
+$app->get("/services/training", function ($req, $res) {
+	return $this->renderer->render($res, "training.phtml");
+});
+
+$app->get("/services/development", function ($req, $res) {
+	return $this->renderer->render($res, "development.phtml");
+});
+
+$app->get("/services/outsourcing", function ($req, $res) {
+	return $this->renderer->render($res, "outsourcing.phtml");
+});
+
 $app->get("/products", function ($req, $res) {
 	return $this->renderer->render($res, "products.phtml");
+});
+
+$app->get("/products/b4c", function ($req, $res) {
+	return $this->renderer->render($res, "b4c.phtml");
+});
+
+$app->get("/products/erp", function ($req, $res) {
+	return $this->renderer->render($res, "erp.phtml");
+});
+
+$app->get("/products/dpl", function ($req, $res) {
+	return $this->renderer->render($res, "dpl.phtml");
 });
 
 $app->get("/projects", function ($req, $res) {
